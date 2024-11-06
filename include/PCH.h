af0b71d19ade9430e4aec618b941e386535ca13e
ZeCroque <zobilezeub@gmail.com>
2024-11-06 14:00:56 +0100

    Repository init

---
+++ include/PCH.h
@@ -0,0 +1,152 @@
+#pragma once
+
+/* +++++++++++++++++++++++++ C++23 Standard Library +++++++++++++++++++++++++ */
+
+// C standard library headers
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <cinttypes>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+
+// C++ standard library headers
+#include <algorithm>
+#include <any>
+#include <array>
+#include <atomic>
+#include <barrier>
+#include <bit>
+#include <bitset>
+#include <charconv>
+#include <chrono>
+#include <compare>
+#include <complex>
+#include <concepts>
+#include <condition_variable>
+#include <coroutine>
+#include <deque>
+#include <exception>
+#include <execution>
+#include <expected>
+#include <filesystem>
+#include <format>
+#include <forward_list>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <latch>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <mdspan>
+#include <memory>
+#include <memory_resource>
+#include <mutex>
+#include <new>
+#include <numbers>
+#include <numeric>
+#include <optional>
+#include <ostream>
+#include <print>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <semaphore>
+#include <set>
+#include <shared_mutex>
+#include <source_location>
+#include <span>
+#include <spanstream>
+#include <sstream>
+#include <stack>
+#include <stacktrace>
+#include <stdexcept>
+#include <stdfloat>
+#include <stop_token>
+#include <streambuf>
+#include <string>
+#include <string_view>
+#include <syncstream>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <variant>
+#include <vector>
+#include <version>
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+#include <RE/Starfield.h>
+#include <REL/REL.h>
+#include <SFSE/SFSE.h>
+
+#include "Plugin.h"
+
+using namespace std::literals;
+using namespace REL::literals;
+
+namespace logger = SFSE::log;
+namespace stl    = SFSE::stl;
+
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using i8  = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+template <typename T>
+class Singleton
+{
+protected:
+    constexpr Singleton() noexcept  = default;
+    constexpr ~Singleton() noexcept = default;
+
+public:
+    constexpr Singleton(const Singleton&)      = delete;
+    constexpr Singleton(Singleton&&)           = delete;
+    constexpr auto operator=(const Singleton&) = delete;
+    constexpr auto operator=(Singleton&&)      = delete;
+
+    [[nodiscard]] static constexpr auto GetSingleton() noexcept
+    {
+        static T singleton;
+        return std::addressof(singleton);
+    }
+};
---
+++ src/Main.cpp
@@ -0,0 +1,28 @@
+#include "Hooks.h"
+#include "Settings.h"
+
+// SFSE message listener, use this to do stuff at specific moments during runtime
+void Listener(SFSE::MessagingInterface::Message* message) noexcept
+{
+    if (message->type == SFSE::MessagingInterface::kPostDataLoad) {
+        Settings::LoadSettings();
+        Hooks::Install();
+    }
+}
+
+// Main SFSE plugin entry point, initialize everything here
+SFSEPluginLoad(const SFSE::LoadInterface* sfse)
+{
+    Init(sfse);
+
+    logger::info("{} {} is loading...", Plugin::Name, Plugin::Version.string());
+
+    if (const auto messaging{ SFSE::GetMessagingInterface() }; !messaging->RegisterListener(Listener)) {
+        return false;
+    }
+
+    logger::info("{} has finished loading.", Plugin::Name);
+    logger::info("");
+
+    return true;
+}
